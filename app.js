8dacf0e9a2243e8010825010413b4c10a662cb57
Francieli Sauthier <francieli.sauthier@gmail.com>
2023-04-22 21:01:25 +0200

    done bonus iterations and some style

--- app.js
+++ app.js
@@ -14,27 +14,44 @@ app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/beers', (req, res) => {
-  punkAPI
-    .getBeers()
-    .then(beersFromApi => {
-      res.render('beers', { beers: beersFromApi });
-    })
-    .catch(error => console.log(error));
+// getBeers() is a method on the punkAPI and it'll return all the beers
+app.get('/beers', async (req, res) => {
+  try {
+    const beersFromApi = await punkAPI.getBeers();
+    res.render('beers', { beersFromApi });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get('/random-beer', (req, res) => {
-  punkAPI
-    .getRandom()
-    .then(RandomBeerFromApi => {
-      res.render('random-beer', { randomBeer: RandomBeerFromApi });
-    })
-    .catch(error => console.log(error));
+// getRandom() is a method on the punkAPI and it'll return an array with a random beer, this is the reason for put await between () and [0]
+app.get('/random-beer', async (req, res) => {
+  try {
+    const randomBeerFromApi = (await punkAPI.getRandom())[0];
+    res.render('randomBeer', { randomBeerFromApi });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// dynamic route - grab information from url
+app.get('/beer-detail/:beerId', async (req, res) => {
+  try {
+    const myBeerId = req.params.beerId;
+    console.log('my params: ', req.params, 'my id only: ', myBeerId);
+
+    // getBeer() return an array, this is the reason for put await between () and [0]
+    const clickedBeer = (await punkAPI.getBeer(myBeerId))[0];
+    console.log('here is the one: ', clickedBeer);
+
+    res.render('beerDetail', { clickedBeer });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
